1997a88b069fc54b5b33b97786e94ce432ea5e99
Atefeh Masoumi <atefeh.masoomy@gmail.com>
2022-06-16 12:59:14 +0430

    profile page right side bar

--- src/pages/Profilepage/ProfilePage.js
+++ src/pages/Profilepage/ProfilePage.js
@@ -3,9 +3,9 @@ import cancelshop from '../../asset/cancelshop.png';
 import "./profilePage.css";
 import { FaRegUserCircle } from "react-icons/fa";
 import { useAuth } from "../../Providers/AuthProvider";
-import {AiOutlineShopping, AiOutlineLogout, AiOutlineUser } from "react-icons/ai";
+import {AiOutlineShopping, AiOutlineLogout, AiOutlineUser,AiOutlineEdit } from "react-icons/ai";
 import { useState } from 'react';
-
+import { IoIosArrowDropright } from "react-icons/io";
 const ProfilePage = ({history}) => {
   const [Show, setShow] = useState(true)
     const userInfo = useAuth();
@@ -34,29 +34,43 @@ const ProfilePage = ({history}) => {
           <div className="profileContainer d-flex flex-row ">
             <div className="col-md-4 d-flex justify-content-center">
               <div className="info-section d-flex flex-column justify-content-center ">
-                <div className="user-info m-2 ">
-                 <div className="d-flex align-items-center">
-                  <FaRegUserCircle fontSize="2em" color="#000" className="usericon" />
-                  <p className="user-name fs-5 p-2 pt-4 ">Dear Customer{userInfo.name} </p>
+                <div className="user-info m-2 d-flex ">
+                 <div className="d-flex flex-column ">
+                  <div className="d-flex justify-content-between">
+                    <div className="d-flex">
+                    <FaRegUserCircle fontSize="2em" color="#000" className="usericon" />
+                    <p className="user-name fs-5 p-2 ">Dear Customer{userInfo.name} </p>
+                  </div>
+                  <div className='d-flex'>
+                    <AiOutlineEdit fontSize="2em" color="#000" className='d-flex'/>
+                  </div>
+                  </div>
+                  <div className="">
+                  <p className="emailP">Email : {userInfo.email}</p>
+                  </div>
                  </div>
                 </div>
                 <div className="d-flex flex-column justify-content-center m-2">
-                    <div className="position-relative d-flex border-bottom">
+                    <div className="position-relative d-flex border-bottom border-top pt-lg-3 bghover">
                       <AiOutlineShopping  fontSize="2em" color="#000" />
                       <p className="pb-3 fs-5 p-1">Orders</p>
                     </div>
-                    <div className="d-flex border-bottom pt-lg-1">
+                    <div className="d-flex justify-content-between border-bottom pt-lg-3 bghover ">
+                      <div className='d-flex'>
                       <AiOutlineUser fontSize="2em" color="#000"/>
                       <p className="pb-3 fs-5 p-1"> User Info</p>
+                      </div>
+                      <div className="d-flex ">
+                       <IoIosArrowDropright fontSize="2em"/>
+                      </div>
                     </div>
-                    <div className="d-flex pt-lg-1 cursor-pointer">
+                    <div className="d-flex pt-lg-1 cursor-pointer bghover">
                       <AiOutlineLogout fontSize="2em" color="#000"/>
                       <p className="pb-3 fs-5 p-1" onClick={logoutHandler}>Logout</p>
                         
                     </div>
                 </div>
               </div>
-
             </div>
             <div className="col-md-8 d-flex flex-column shopinfo">
               <div className="d-flex flex-column border rounded p-2">
