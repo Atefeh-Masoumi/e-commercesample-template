b88d9e40fb29d97aa3c1c4e7cc2e6c5e2050ec6f
Atefeh Masoumi <atefeh.masoomy@gmail.com>
2022-07-14 12:14:33 +0430

    contactuspage

--- src/pages/ContactUsPage/ContactusPage.js
+++ src/pages/ContactUsPage/ContactusPage.js
@@ -1,9 +1,10 @@
 import Footer from "../../component/Footer/FooterPage";
 import "./contactus.css";
-
+import { BsFillTelephoneFill } from "react-icons/bs";
 const ContactUsPage = () => {
   return (
     <>
+      {/* contact us form */}
       <form className="my-form">
         <div className="container">
           <h1>Get in touch!</h1>
@@ -52,7 +53,7 @@ const ContactUsPage = () => {
                   </span>
                   <span className="front">SUBMIT</span>
                 </button>
-                <button className="btn-grid" type="reset" >
+                <button className="btn-grid" type="reset">
                   <span className="back">
                     <img
                       src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/eraser-icon.svg"
@@ -66,7 +67,27 @@ const ContactUsPage = () => {
           </ul>
         </div>
       </form>
-      <Footer/>
+
+      {/* google map */}
+      <div className="d-flex justify-content-center m-4">
+        <div class="borderanimation">
+          <h3>We are at your Service</h3>
+          <div class="d-flex flex-column">
+            <div><BsFillTelephoneFill /> call us at 0000000</div>
+            <div>Email us at lorem@gmail.com</div>
+          </div>
+        </div>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15624092.116566982!2d-88.93636968409682!3d36.4918863647972!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2ssg!4v1657778117750!5m2!1sen!2ssg"
+          width="600"
+          height="450"
+          style={{ border: "0" }}
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+      <Footer />
     </>
   );
 };
