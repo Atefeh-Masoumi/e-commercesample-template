b2aef163a067266bf57301acd277ad7036063f3f
Atefeh Masoumi <atefeh.masoomy@gmail.com>
2022-07-21 12:44:14 +0430

    inputform updated

--- src/component/Login/login.js
+++ src/component/Login/login.js
@@ -1,90 +1,107 @@
 import Input from "../../common/Input";
 import { useFormik } from "formik";
-import * as Yup from 'yup';
-import './login.css';
-import { Link,useNavigate } from "react-router-dom";
-import { useState ,useEffect} from "react";
+import * as Yup from "yup";
+import "./login.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { loginUser } from "../../Services/LoginService";
-import { useAuthAction,useAuth } from "../../Providers/AuthProvider";
-import {useQuery} from './../../hooks/useQuery'
+import { useAuthAction, useAuth } from "../../Providers/AuthProvider";
+import { useQuery } from "./../../hooks/useQuery";
 import { toast } from "react-toastify";
 
 // inital values for the form
 const initialValues = {
-    email: "",
-    password: "",
+  email: "",
+  password: "",
 };
 
 // Yup schema for form validation
 const validationSchema = Yup.object({
-    email: Yup.string().email("Invalid email format").required("Email is required"),
-    password: Yup.string().required("password is required"),
+  email: Yup.string()
+    .email("Invalid email format")
+    .required("Email is required"),
+  password: Yup.string().required("password is required"),
 });
-
 
 const LoginForm = () => {
   // error state
-    const[err, setError]= useState(null);
-    const history = useNavigate()
-    const query = useQuery();
-    const redirect = query.get('redirect') ||'/';
+  const [err, setError] = useState(null);
+  const history = useNavigate();
+  const query = useQuery();
+  const redirect = query.get("redirect") || "/";
 
-    // useAuthActions to dispatch actions
-     const setAuth = useAuthAction();
-     const userData = useAuth();
-       
-    useEffect(()=>{
-        if(userData) history(redirect);
-    },[redirect,userData,history])
+  // useAuthActions to dispatch actions
+  const setAuth = useAuthAction();
+  const userData = useAuth();
 
-      // submit handler
-    const onSubmit= async(values)=>{
-       
-        try {
-         localStorage.setItem("auth token", JSON.stringify(values));  
-           console.log(values);   
-            // if you want to use API uncomment line blew
-            //const {data} = await loginUser(values)
-            setAuth(values);
-            setError(null);
-            history("/");             
+  useEffect(() => {
+    if (userData) history(redirect);
+  }, [redirect, userData, history]);
 
-        } catch (error) {
-            console.log(error);
-            if(error.response && error.response.data.message)
-            setError(error.response.data.message);
-            toast.error(error.response.data.message);
-        }
-    };
+  // submit handler
+  const onSubmit = async (values) => {
+    try {
+      localStorage.setItem("auth token", JSON.stringify(values));
+      console.log(values);
+      // if you want to use API uncomment line blew
+      //const {data} = await loginUser(values)
+      setAuth(values);
+      setError(null);
+      history("/");
+    } catch (error) {
+      console.log(error);
+      if (error.response && error.response.data.message)
+        setError(error.response.data.message);
+      toast.error(error.response.data.message);
+    }
+  };
 
+  // useFormik hook
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validationSchema,
+    validateOnMount: true,
+  });
 
-    // useFormik hook
-    const formik = useFormik({
-        initialValues,
-        onSubmit,
-        validationSchema,
-        validateOnMount: true,
-    });
+  return (
+    <div className="formContainer mb-8">
+      <form onSubmit={formik.handleSubmit} className="borderanimation m-5 p-5 rounded h-100">
+        <Input formik={formik} name="email" label="Email" type="email" />
+        <Input
+          formik={formik}
+          name="password"
+          label="Password"
+          type="password"
+          autocomplete="on"
+        />
 
-    return (
-        <div className="formContainer">
-            <form onSubmit={formik.handleSubmit}>
-                <Input formik={formik} name="email" label="Email" type="email" />
-                <Input formik={formik} name="password" label="Password" type='password' autocomplete="on" />
-                
-                <button type="submit" disabled={!formik.isValid} className="btn primary btnlg button-addtocart" >   
-                    Login 
-                </button>   
+        <button
+          type="submit"
+          disabled={!formik.isValid}
+          className="border-0 outline-none rounded borderbtn-animation"
+        >
+          <span>
+            <span>
+              <span>Login </span>
+            </span>
+          </span>
+        </button>
 
-                {err&&<p style={{color:"red",marginTop:"20px"}}> {err}</p>}
+        {err && <p style={{ color: "red", marginTop: "20px" }}> {err}</p>}
 
-                <Link to={`/signup`}>
-                    <p className="">Not have an Account yet? </p>
-                </Link>            
-
-            </form>
-        </div>
-    );
-}
+        <Link to={`/signup`}>
+          <button className="border-0 outline-none rounded borderbtn-animation ms-4">
+          <span>
+            <span>
+              <span>signup</span>
+            </span>
+          </span>
+          </button>
+        </Link>
+      </form>
+    </div>
+  );
+};
 
 export default LoginForm;
--- src/component/SignUp/SignUp.js
+++ src/component/SignUp/SignUp.js
@@ -1,105 +1,137 @@
 import Input from "../../common/Input";
 import { useFormik } from "formik";
-import * as Yup from 'yup'; 
-import './signup.css';
-import { Link,useNavigate } from "react-router-dom";
+import * as Yup from "yup";
+import "./signup.css";
+import { Link, useNavigate } from "react-router-dom";
 import { signupUser } from "../../Services/SignUpService";
-import { useState,useEffect } from "react";
-import { useAuthAction,useAuth } from "../../Providers/AuthProvider";
-import {useQuery} from './../../hooks/useQuery'
+import { useState, useEffect } from "react";
+import { useAuthAction, useAuth } from "../../Providers/AuthProvider";
+import { useQuery } from "./../../hooks/useQuery";
 
 // inital values for the form
-const initialValues={
-    name:"",
-    email:"",
-    phoneNumber:"",
-    password:"",
-    passwordconfirm:"",
+const initialValues = {
+  name: "",
+  email: "",
+  phoneNumber: "",
+  password: "",
+  passwordconfirm: "",
 };
 // Yup schema for form validation
 const validationSchema = Yup.object({
-    name:Yup.string().required("Name is required").min(4,"Name lenght is not Valid"),
-    email:Yup.string().email("Invalid email format").required("Email is required"),
-    phoneNumber: Yup.string()
+  name: Yup.string()
+    .required("Name is required")
+    .min(4, "Name lenght is not Valid"),
+  email: Yup.string()
+    .email("Invalid email format")
+    .required("Email is required"),
+  phoneNumber: Yup.string()
     .required("Phone Number is required")
     .matches(/^[0-9]{11}$/, "Invalid Phone Number")
     .nullable(),
-    password: Yup.string()
+  password: Yup.string()
     .required("Password is required")
     .matches(
       /^(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
       "Password must contain at least 8 characters,number and special character"
     ),
-    passwordconfirm:Yup.string().required("password confirmation required").oneOf([Yup.ref('password'),null], 'Password must match'),
+  passwordconfirm: Yup.string()
+    .required("password confirmation required")
+    .oneOf([Yup.ref("password"), null], "Password must match"),
 });
 
-
 const SignupForm = () => {
-    // useAuthActions to dispatch actions and useAuth to get the auth state
-    const setAuth=useAuthAction();
-    const history = useNavigate();
-    // useQuery hook to get the query params and redirect
-     const query = useQuery();
-     const redirect = query.get('redirect') || "/";
+  // useAuthActions to dispatch actions and useAuth to get the auth state
+  const setAuth = useAuthAction();
+  const history = useNavigate();
+  // useQuery hook to get the query params and redirect
+  const query = useQuery();
+  const redirect = query.get("redirect") || "/";
 
-      // error state
-    const [error, setError]=useState(null);
-    const userData = useAuth();
+  // error state
+  const [error, setError] = useState(null);
+  const userData = useAuth();
 
-// useEffect to check if the user is already logged in
-    useEffect(()=>{
-        if(userData) history(redirect);
-    },[redirect,userData,history]);
+  // useEffect to check if the user is already logged in
+  useEffect(() => {
+    if (userData) history(redirect);
+  }, [redirect, userData, history]);
 
-    // submit handler
-    const onSubmit= async(values)=>{
-        const {name,email,phoneNumber,password} = values;
-        const userData={
-            name,
-            email,
-            phoneNumber,
-            password,
-        };
-        try {
-           const {data}= await signupUser(userData);
-           setAuth(data);
-            localStorage.setItem("auth token", JSON.stringify(data));
-           console.log(data);
-           setError(null);
-           history(redirect);
-           
-        } catch (error) {
-            console.log(error);
-            if(error.response && error.response.data.message)
-            setError(error.response.data.message);
-        }
+  // submit handler
+  const onSubmit = async (values) => {
+    const { name, email, phoneNumber, password } = values;
+    const userData = {
+      name,
+      email,
+      phoneNumber,
+      password,
     };
+    try {
+      const { data } = await signupUser(userData);
+      setAuth(data);
+      localStorage.setItem("auth token", JSON.stringify(data));
+      console.log(data);
+      setError(null);
+      history(redirect);
+    } catch (error) {
+      console.log(error);
+      if (error.response && error.response.data.message)
+        setError(error.response.data.message);
+    }
+  };
 
-      // useFormik hook
-    const formik = useFormik({
-        initialValues,
-        onSubmit,
-        validationSchema,
-        validateOnMount: true,
-      });
-    return ( 
-        <div className="formContainer">
-            <form onSubmit={formik.handleSubmit} >
-                <Input formik={formik} name="name" label="Name" />
-                <Input formik={formik} name="email" label="Email" type="email" />
-                
-                <Input  formik={formik} name="phoneNumber" label="Number" type="tel"/>
-                <Input  formik={formik} name="password" label="Password" type='password' autocomplete="on"/>
-                <Input  formik={formik} name="passwordconfirm" label="Password Confirm" type='password'/>
-                <button type="submit" disabled={!formik.isValid} className="btn primary btnsg" > Sign Up</button>
-                {error&&<p className="error" > {error}</p>}
-                
-                <Link to={`/login?redirect=${redirect}`}>
-                    <p className="alreadylogin">Already have an account? Login from here</p>
-                </Link>
-            </form>
-        </div>
-    );
-}
- 
+  // useFormik hook
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validationSchema,
+    validateOnMount: true,
+  });
+  return (
+    <div className="formContainer">
+      <form
+        onSubmit={formik.handleSubmit}
+        className="borderanimation m-5 p-5 rounded h-100"
+      >
+        <Input formik={formik} name="name" label="Name" />
+        <Input formik={formik} name="email" label="Email" type="email" />
+
+        <Input formik={formik} name="phoneNumber" label="Number" type="tel" />
+        <Input
+          formik={formik}
+          name="password"
+          label="Password"
+          type="password"
+          autocomplete="on"
+        />
+        <Input
+          formik={formik}
+          name="passwordconfirm"
+          label="Password Confirm"
+          type="password"
+        />
+        <button
+          type="submit"
+          disabled={!formik.isValid}
+          className=" border-0 outline-none rounded borderbtn-animation ms-4"
+        >
+          
+            <span>
+              <span>
+                <span>signup</span>
+              </span>
+            </span>
+          </button>
+        
+        {error && <p className="error"> {error}</p>}
+
+        <Link to={`/login?redirect=${redirect}`}>
+          <p className="alreadylogin">
+            Already have an account? Login from here
+          </p>
+        </Link>
+      </form>
+    </div>
+  );
+};
+
 export default SignupForm;
